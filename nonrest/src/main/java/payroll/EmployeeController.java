0c307d008a9362b4fa6cb2c0ff7c5f940b42e4b7
shraddhayelamarthy <109479657+shraddhayelamarthy@users.noreply.github.com>
2024-01-09 14:23:21 +0530

    shraddha committed

--- nonrest/src/main/java/payroll/EmployeeController.java
+++ nonrest/src/main/java/payroll/EmployeeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-//selab evaluation shraddha comment
+//selab evaluation shraddha comment changed
 @RestController
 class EmployeeController {
 
